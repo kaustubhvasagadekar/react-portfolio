982d671183c85c81a2b4859b2986d6af14b7e2f5
kaustubhvasagadekar <kaustubh111111@gmail.com>
2020-03-15 16:15:13 +0530

    commit on master after some miner changes to index.html

---
+++ build/precache-manifest.de492712b5e3b16fbf29061e108d3f3f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61ecbb824fc5d0967c1762988e7c3e50",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e8044054e7469643fd13",
+    "url": "/static/css/main.bd95a0c8.chunk.css"
+  },
+  {
+    "revision": "0aab1e5bfe140c097747",
+    "url": "/static/js/2.77c1a0f1.chunk.js"
+  },
+  {
+    "revision": "e8044054e7469643fd13",
+    "url": "/static/js/main.f468ecc1.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);

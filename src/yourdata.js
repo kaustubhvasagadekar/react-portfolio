b665cba5f2cbc9a71311f78a4b7c2835733c33da
kaustubhvasagadekar <kaustubh111111@gmail.com>
2020-03-15 15:32:22 +0530

    Add files via upload

---
+++ src/yourdata.js
@@ -0,0 +1,79 @@
+export default
+    {
+        //(Please Do Not Remove The comma(,) after every variable)
+        //Change The Website Template
+        name :'kaustubh vasagadekar.',
+        headerTagline: [//Line 1 For Header
+                        'Hi 👋 You found me! I am Kaustubh vasagadekar.',
+                        //Line 2 For Header
+                        'UI designer ',
+                        //Line 3 For Header
+                        'from India'
+    ],
+        //Contact Email
+        contactEmail:'kaustubh.vasagadekar10.com',
+        // Add Your About Text Here
+        abouttext: ".",
+        aboutImage:'https://i.pinimg.com/originals/92/88/59/9288597ec595b48aa1907d47eefd5047.jpg',
+       //Change This To Hide The Image of About Section (True Or False)
+       ShowAboutImage:true,//true or false (Change Here)
+      
+      
+      
+       // Change Projects Here 
+       projects:[
+           {
+            id: 1,//DO NOT CHANGE THIS (Please)😅
+            title:'def', //Project Title - Add Your Project Title Here
+             service:'UI/UX Design', // Add Your Service Type Here
+            //Project Image - Add Your Project Image Here
+             imageSrc:"https://images.unsplash.com/photo-1558104631-0fa41a8f6c20?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+             //Project URL - Add Your Project Url Here
+             url:''
+            },
+            {
+                id: 2,//DO NOT CHANGE THIS (Please)😅
+                title: 'def 2',
+                service: 'Website Development',
+                imageSrc: "https://images.unsplash.com/photo-1558104631-0fa41a8f6c20?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+                url: ''
+            },
+    
+
+                    /*
+
+                    If You Want To Add More Project just Copy and Paste This At The End (Update the id Respectively)
+                ,{
+                id: 5,
+                title: 'Project Five',
+                service: 'Something Amazing',
+                imageSrc: "",
+                url: ''
+            }
+                */
+        ],
+        social: [
+            // Add Or Remove The Link Accordingly
+            {   name:'Github',
+                url:'https://github.com/kaustubhvasagadekar'},
+            // {
+            //     name: 'Behance',
+            //     url: 'https://www.behance.net/'
+            // },
+            // {
+            //     name: 'Dribbble',
+            //     url: 'https://dribbble.com/'
+            // },
+            {
+                name: 'Instagram',
+                url: 'https://www.instagram.com/kaustubh.s.vasagadekar/'
+            },
+            {
+                name: 'Instagram',
+                url: 'https://www.instagram.com/kaustubh_spade/'
+            },
+            {   name:'Youtube',
+            url:'https://youtube.com/'},
+
+        ]
+    }
